a6bad4732aa8552a01d6089ebe56b80a24e470b6
sarKarji1 <149999688+sarKarji1@users.noreply.github.com>
2025-02-25 15:52:49 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,6 +1,6 @@
 const mega = require("megajs");
 const auth = {
-    email: 'hiranrahul98@gmail.com',
+    email: 'rahulhiran654@gmail.com',
     password: '@rahul9356',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
